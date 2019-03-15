9335e317eb55e5121555185d30a211efcd8af7b3
chrisberry4545 <chrisberry4545@googlemail.com>
2019-03-15 08:38:29 +0000

    Updates

---
+++ precache-manifest.47267b29b0b9f511b3a98e0896aa0c7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5257a9274660698339ec",
+    "url": "/static/js/main.5257a927.chunk.js"
+  },
+  {
+    "revision": "64e3cc46a626655a9b36",
+    "url": "/static/js/2.64e3cc46.chunk.js"
+  },
+  {
+    "revision": "5257a9274660698339ec",
+    "url": "/static/css/main.ec6db74f.chunk.css"
+  },
+  {
+    "revision": "a54d21f8903c9fc8cbe266148e09da19",
+    "url": "/index.html"
+  }
+];
