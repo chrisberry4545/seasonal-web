b8d1a8ecb3ad8b0752b40210de81af131b35788b
chrisberry4545 <chrisberry4545@googlemail.com>
2019-03-21 19:06:42 +0000

    updated to use specific api version

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 const BACKEND_URL = process.env.BACKEND_URL
-    || 'https://6usraevcmc.execute-api.eu-west-2.amazonaws.com/prod';
+    || 'https://6usraevcmc.execute-api.eu-west-2.amazonaws.com/prod/v1';
 
 export const SEASON_DATA_URL =
   process.env.SEASON_DATA_URL || `${BACKEND_URL}/season-data`;
