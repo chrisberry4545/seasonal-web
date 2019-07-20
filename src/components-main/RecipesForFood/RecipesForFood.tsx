cffbb6baea68bcf934b060b07b20bfb5edc590ad
chrisberry4545 <chrisberry4545@googlemail.com>
2019-07-20 14:42:28 +0100

    added dietary requirements to recipes for food

--- src/components-main/RecipesForFood/RecipesForFood.tsx
+++ src/components-main/RecipesForFood/RecipesForFood.tsx
@@ -5,6 +5,7 @@ import './RecipesForFood.scss';
 import { IRecipesForFoodProps } from './RecipesForFood.interface';
 import { ImageGrid } from '../../components-layout';
 import { TextHeadingSmall } from '../../components-elements';
+import { DietaryFiltersConnector } from '../DietaryFilters/DietaryFilters.connector';
 
 export const RecipesForFood: FC<IRecipesForFoodProps> = ({
   isLoading,
@@ -16,6 +17,7 @@ export const RecipesForFood: FC<IRecipesForFoodProps> = ({
       <TextHeadingSmall className='c-recipes-for-food__heading'>
         Recipes
       </TextHeadingSmall>
+      <DietaryFiltersConnector />
       <ImageGrid data={currentFoodDetailsRecipes} onClick={onRecipeSelected} />
     </div>
     : null
